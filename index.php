cc1338e8fa47c79cb66bdb8e72481aa06bebd988
Nali1919 <my@email.com>
2022-10-12 18:38:24 +0200

    Completo esercizio

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-ciao
+<?php
+
+ $frase = ' Ciao mi chiamo Michele e vengo da Cremona';
+
+ echo $frase . '<br>'; 
+
+  echo strlen($frase) . '<br>'; 
+
+ $parola = $_GET["changeParola"];
+
+
+ $change = str_replace('Ciao', $parola, $frase);
+
+ echo $change . '<br>';
+
+ echo strlen($change)
+ 
+
+
+
+
+  
+    
+
+?>
